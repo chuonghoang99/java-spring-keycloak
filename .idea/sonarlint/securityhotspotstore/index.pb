f477c838b7494c44a980551e844725781dbdc0c7
ChuongHoang <>
2024-08-16 14:13:56 +0700

    update security

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -7,3 +7,16 @@
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
 q
 Asrc/main/java/com/devteria/profile/ProfileServiceApplication.java,8/6/8682a41a75e8c81646e0134ec267261e428f1285
+n
+>src/main/java/com/devteria/profile/service/ProfileService.java,9/0/9016461ce465810a151a9e16011958cfeacb5d10
+l
+<src/main/java/com/devteria/profile/mapper/ProfileMapper.java,3/1/31bd1c188d6d76c158c1e4b0bedae09a5fdf78dd
+q
+Asrc/main/java/com/devteria/profile/repository/IdentityClient.java,e/a/eafc8a859f67bbeb56032257417144172f800d51
+t
+Dsrc/main/java/com/devteria/profile/configuration/SecurityConfig.java,8/c/8c383c04c6918645a8e764cc083059e1536c2e32
+t
+Dsrc/main/java/com/devteria/profile/controller/ProfileController.java,6/5/6502ec8be4ab133e95267163e9f020ac80817a62
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/main/java/com/devteria/profile/configuration/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.devteria.profile.configuration;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final String[] PUBLIC_ENDPOINTS = {"/register"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(PUBLIC_ENDPOINTS)
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+
+//        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())
+//                .authenticationEntryPoint(new JwtAuthenticationEntryPoint()));
+
+
+        httpSecurity.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+
+        return httpSecurity.build();
+    }
+}
